2cd5a8743cfde0793282d249f1bac7ce1f3eacea
shraddha <shraddhaturkane3223@gmail.com>
2026-03-20 16:05:13 +0530

    navbar & footer done

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="page">
+      <h1>Login Page</h1>
+      <p>Please log in to your account.</p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div className="page">
+      <h1>Sign Up Page</h1>
+      <p>Create a new account!</p>
+    </div>
+  );
+};
+
+export default SignUp;
